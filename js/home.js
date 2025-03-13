ad76e8017701f2ce31dcddd6ed419559dedbfe44
Khaled Fathi <dev@khaledfathi.com>
2025-03-13 03:46:19 +0200

    add timer

--- js/home.js
+++ js/home.js
@@ -1,21 +1,43 @@
-console.log('home.js loaded');
+// console.log('home.js loaded');
 
-const mouseCircle = document.querySelector('#mouse-circle') ; 
+const 
+    timerSeconeds= document.querySelector('#timer-seconds') ,
+    mouseCircle = document.querySelector('#mouse-circle') ,
+    mouse = {x: 0, y:0},
+    circle = {x:0, y:0},
+    speed = 0.5; 
 
-const mouse = {x: 0, y:0},
-    circle = {x:0, y:0};
+// ##### Events ##### 
+window.addEventListener('mousemove', actionGetMouseCoordinates) ;
+// ##### -END- Events ##### 
 
-window.addEventListener('mousemove', event => {
+// ##### Actions ##### 
+function actionGetMouseCoordinates(event){
    mouse.x = event.x;
    mouse.y = event.y;
-}); 
+}
+// ##### -END- Actions ##### 
 
-const speed = 0.5; 
-const tick = () => {
+// ##### Functions  ##### 
+function tick () {
     circle.x += (mouse.x - circle.x) * speed; 
     circle.y += (mouse.y - circle.y) * speed; 
     mouseCircle.style.transform= `translate(${circle.x}px, ${circle.y}px)`
     window.requestAnimationFrame(tick); 
-}
+}; 
 
+function timer(){
+    var sec = 60;
+    var timer = setInterval(function(){
+        timerSeconeds.innerHTML=sec--;
+        if (sec < 0) sec=60; 
+    }, 1000);
+}
+// ##### -END- Functions ##### 
+
+
+// ##### Invocations ##### 
 tick(); 
+timer(); 
+// ##### -END- Invocations ##### 
+
