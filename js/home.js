1f87e012bd489cbfa0700c83481f66f84c24115d
Khaled Fathi <dev@khaledfathi.com>
2025-03-13 02:56:24 +0200

    add mouse circle

--- js/home.js
+++ js/home.js
@@ -1,1 +1,21 @@
 console.log('home.js loaded');
+
+const mouseCircle = document.querySelector('#mouse-circle') ; 
+
+const mouse = {x: 0, y:0},
+    circle = {x:0, y:0};
+
+window.addEventListener('mousemove', event => {
+   mouse.x = event.x;
+   mouse.y = event.y;
+}); 
+
+const speed = 0.5; 
+const tick = () => {
+    circle.x += (mouse.x - circle.x) * speed; 
+    circle.y += (mouse.y - circle.y) * speed; 
+    mouseCircle.style.transform= `translate(${circle.x}px, ${circle.y}px)`
+    window.requestAnimationFrame(tick); 
+}
+
+tick(); 
