6cfe7117d836e9db72735cfdb7ba00cd243d1d30
Khaled Fathi <dev@khaledfathi.com>
2025-03-15 03:06:18 +0200

    check point

--- js/home.js
+++ js/home.js
@@ -1,42 +1,54 @@
 // console.log('home.js loaded');
 
-const 
-    timerSeconeds= document.querySelector('#timer-seconds') ,
-    mouseCircle = document.querySelector('#mouse-circle') ,
-    mouse = {x: 0, y:0},
-    circle = {x:0, y:0},
-    speed = 0.5; 
 
-// ##### Events ##### 
-window.addEventListener('mousemove', actionGetMouseCoordinates) ;
-// ##### -END- Events ##### 
+// ##### Elements #####
+const
+    timerSeconeds = document.querySelector("#timer-seconds"),
+    collectionText = document.querySelector("#collection-play-text"),
+    mouseCircle = document.querySelector("#mouse-circle"); 
 
-// ##### Actions ##### 
-function actionGetMouseCoordinates(event){
-   mouse.x = event.x;
-   mouse.y = event.y;
+// ##### vars #####
+const
+    mouse = { x: 0, y: 0 },
+    circle = { x: 0, y: 0 },
+    speed = 0.5;
+
+
+// ##### Functions  #####
+// traking circle element under mouse movement 
+function tick() {
+  circle.x += (mouse.x - circle.x) * speed;
+  circle.y += (mouse.y - circle.y) * speed;
+  mouseCircle.style.transform = `translate(${circle.x}px, ${circle.y}px)`;
+  window.requestAnimationFrame(tick);
 }
-// ##### -END- Actions ##### 
 
-// ##### Functions  ##### 
-function tick () {
-    circle.x += (mouse.x - circle.x) * speed; 
-    circle.y += (mouse.y - circle.y) * speed; 
-    mouseCircle.style.transform= `translate(${circle.x}px, ${circle.y}px)`
-    window.requestAnimationFrame(tick); 
-}; 
-
-function timer(){
-    var sec = 60;
-    var timer = setInterval(function(){
-        timerSeconeds.innerHTML=sec--;
-        if (sec < 0) sec=60; 
-    }, 1000);
+// count 1 min repeatedly, applied on timerSeconds element 
+function timer() {
+  var sec = 60;
+  var timer = setInterval(function () {
+    timerSeconeds.innerHTML = sec--;
+    if (sec < 0) sec = 60;
+  }, 1000);
 }
-// ##### -END- Functions ##### 
 
+// ##### Events #####
+window.addEventListener("mousemove", actionGetMouseCoordinates);
 
-// ##### Invocations ##### 
+// ##### Actions #####
+// update mouse coordinates
+function actionGetMouseCoordinates(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+}
+
+// ##### Manipulations #####
+// for [collection text] rotation animation  
+collectionText.innerHTML = collectionText.innerText.split('').map(
+    (char , index) => `<span style="transform:rotate(${index * 14.5}deg)">${char}</span>`
+).join('');
+console.log(collectionText);
+
+// ##### Invocations #####
 tick(); 
-timer(); 
-// ##### -END- Invocations ##### 
+timer();
